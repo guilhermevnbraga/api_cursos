bfd7a0be9cec6fa29d89a91136ae099d591406f6
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2025-01-27 22:20:11 -0300

    feat: Add Course delete and exceptions

--- src/main/java/br/com/guilhermevnbraga/api_cursos/modules/course/controllers/CourseController.java
+++ src/main/java/br/com/guilhermevnbraga/api_cursos/modules/course/controllers/CourseController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.guilhermevnbraga.api_cursos.exceptions.CourseNotFoundException;
 import br.com.guilhermevnbraga.api_cursos.modules.course.repositories.CourseRepository;
 import br.com.guilhermevnbraga.api_cursos.modules.course.useCases.UpdateCourseUseCase;
 
@@ -50,6 +52,19 @@ public class CourseController {
     try {
       var result = this.updateCourseUseCase.execute(id, courseEntity);
       return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @DeleteMapping("courses/{id}")
+  public ResponseEntity<Object> delete(@PathVariable UUID id) {
+    try {
+      if (!courseRepository.existsById(id)) {
+        throw new CourseNotFoundException();
+      }
+      courseRepository.deleteById(id);
+      return ResponseEntity.ok().body("Course deleted successfully.");
     } catch (Exception e) {
       return ResponseEntity.badRequest().body(e.getMessage());
     }
